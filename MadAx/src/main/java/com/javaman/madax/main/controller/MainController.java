28f3a454be1b3cf06d1fdff2b194770067190e14
David <yhoon_0802@hotmail.com>
2023-10-27 17:44:09 +0900

    20231072 로그인페이지

--- MadAx/src/main/java/com/javaman/madax/main/controller/MainController.java
+++ MadAx/src/main/java/com/javaman/madax/main/controller/MainController.java
@@ -2,9 +2,11 @@ package com.javaman.madax.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("member")
 public class MainController {
 
 	@RequestMapping("/")
@@ -12,4 +14,12 @@ public class MainController {
 		return "common/main";
 	}
 	
+	
+	/** 로그인 전용 페이지 forward
+	 * @return "member/login"
+	 */
+	@GetMapping("login")
+	public String login() {
+		return "member/login";
+	}
 }
