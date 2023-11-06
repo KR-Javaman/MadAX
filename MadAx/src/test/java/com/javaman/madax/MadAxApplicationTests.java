0ab3d1a99e1a0c4aa2af4f23d5592a70acfe0d5f
momo <rhwoah7190@gmail.com>
2023-11-06 14:50:57 +0900

    복구

---
+++ MadAx/src/test/java/com/javaman/madax/MadAxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaman.madax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadAxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
