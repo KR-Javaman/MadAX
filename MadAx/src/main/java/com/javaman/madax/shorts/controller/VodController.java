b1a531ff8beaea972ae71cc4465cceb713f72849
momo <rhwoah7190@gmail.com>
2023-11-06 17:51:10 +0900

    쇼츠 부분 백엔드 + 프론트

---
+++ MadAx/src/main/java/com/javaman/madax/shorts/controller/VodController.java
@@ -0,0 +1,8 @@
+package com.javaman.madax.shorts.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class VodController {
+
+}
---
+++ MadAx/src/main/java/com/javaman/madax/shorts/model/service/VodServiceImpl.java
@@ -0,0 +1,8 @@
+package com.javaman.madax.shorts.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VodServiceImpl implements VodService{
+
+}
