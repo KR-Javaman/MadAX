671ea1eb3baec2613ce0a16f2773d6c8dbac5050
David <yhoon_0802@hotmail.com>
2023-11-08 14:59:26 +0900

    231108 로그인, 회원가입, 마이페이지 수정

--- MadAx/src/main/java/com/javaman/madax/myPage/controller/MyPageController.java
+++ MadAx/src/main/java/com/javaman/madax/myPage/controller/MyPageController.java
@@ -52,6 +52,65 @@ public class MyPageController {
 	}
 
 
+	
+	@PostMapping("changeNickname")
+	public String changeNickname(Member updateMember, @SessionAttribute("loginMember") Member loginMember, RedirectAttributes ra) {
+		
+		updateMember.setMemberNo( loginMember.getMemberNo());
+		
+		int result = service.changeNickname(updateMember);
+		
+		String message = null;
+		
+		if(result > 0) {
+			message = "닉네임이 수정되었습니다.";
+			loginMember.setMemberNickname(updateMember.getMemberNickname());
+		}
+		
+		else { 
+			message = "회원 정보 수정 실패..";
+			
+		}
+		
+		ra.addFlashAttribute("message", message);
+		
+		return "redirect:myPage-profile";
+	}
+	
+	
+	@PostMapping("changePw")
+	public String changePw(String currentPw, String newPw,
+			@SessionAttribute("loginMember") Member loginMember, 
+			RedirectAttributes ra) {
+	
+		int memberNo = loginMember.getMemberNo();
+		
+		int result = service.changePw(currentPw, newPw, memberNo);
+		
+		String path = null;
+		String message = null;
+		
+		if(result > 0) {
+			message="비밀번호 수정이 완료되었습니다!";
+			path="redirect:myPage-profile";
+			
+		}
+		
+		else {
+			
+			message= "현재 비밀번호가 일치하지 않습니다.";
+			path = "redirect:myPage-profile";
+		}
+		
+		ra.addFlashAttribute("message",message);
+		return path;
+	}
+	
+	
+	
+	
+	
+
 	@PostMapping("secession")
 	public String secession(String memberPw, @SessionAttribute("loginMember") Member loginMember, 
 			RedirectAttributes ra, SessionStatus status) {
@@ -78,8 +137,6 @@ public class MyPageController {
 		
 		return path;
 	}
-	
-	
 	
 }
 
